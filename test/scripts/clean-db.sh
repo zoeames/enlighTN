01cc0d3871c5d99c4b56947a085a9ffb3bfa3e9e
Zoe Ames <zoeames@gmail.com>
2014-09-13 15:40:09 -0500

    historic locations added

--- test/scripts/clean-db.sh
+++ test/scripts/clean-db.sh
@@ -5,8 +5,4 @@ if [ -z "$1" ] ; then
       exit 1
     fi
 
-    mongoimport --jsonArray --drop --db $1 --collection historicMarkers --file ../../db/historic.json
-    mongoimport --jsonArray --drop --db $1 --collection metroGalleries --file ../../db/galleries.json
-    mongoimport --jsonArray --drop --db $1 --collection metroInstallments --file ../../db/installments.json
-    mongoimport --jsonArray --drop --db $1 --collection civicArt --file ../../db/civic.json
-    mongoimport --jsonArray --drop --db $1 --collection events --file ../../db/events.json
+    mongoimport --jsonArray --drop --db $1 --collection locations --file ../../db/locations.json
