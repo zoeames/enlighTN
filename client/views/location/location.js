796013ba838ab9f6f14db5fe59941e73f267816b
Kayla Jones <KaylaLyne424@gmail.com>
2014-09-14 16:10:52 -0500

    deleted unnecessary slider code

--- client/views/location/location.js
+++ client/views/location/location.js
@@ -7,21 +7,6 @@
     $scope.occasions = [];
     $scope.myInterval = 10000;
 
-    $scope.slides = [
-      {
-        image: 'http://lorempixel.com/400/200/'
-      },
-      {
-        image: 'http://lorempixel.com/400/200/food'
-      },
-      {
-        image: 'http://lorempixel.com/400/200/sports'
-      },
-      {
-        image: 'http://lorempixel.com/400/200/people'
-      }
-    ];
-
     Location.findById($routeParams.locId).then(function(response){
       $scope.loc = response.data.loc;
       $scope.occasions = response.data.occasions;
