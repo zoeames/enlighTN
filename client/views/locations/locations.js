2833a8f410f05c9076d67468a4e34dba98e38334
Brian Hiatt <brian.hiatt41@gmail.com>
2014-09-16 18:58:13 -0500

    refactored map code

--- client/views/locations/locations.js
+++ client/views/locations/locations.js
@@ -3,8 +3,9 @@
 
   angular.module('enlighTN')
   .controller('LocationIndexCtrl', ['$scope', 'Location', function($scope, Location){
-    $scope.title = 'Locations Index Page!';
+    $scope.title     = 'Locations Index Page!';
     $scope.locations = [];
+    $scope.pos       = {lat: 36.1667, lng: -86.7833};
 
     Location.all().then(function(response){
       $scope.locations = response.data.locations;
