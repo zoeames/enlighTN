23b79cd361980363a0a0bd1c9abe391e7bbf24c1
Kayla Jones <KaylaLyne424@gmail.com>
2014-09-18 18:12:20 -0500

    nav

--- client/components/directives/en-table/en-table.js
+++ client/components/directives/en-table/en-table.js
@@ -8,7 +8,10 @@
     var data = [];
     $scope.tableParams = new ngTableParams({
       page: 1,            // show first page
-      count: 10           // count per page
+      count: 10,           // count per page
+      sorting:{
+        name: 'asc'     // initial sorting
+      }
     },{
       total: data.length, // length of data
       getData: function($defer, params){
