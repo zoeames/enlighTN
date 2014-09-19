9a1409535c80eaf57da8c616fd0f5ef58726db9b
Zoe Ames <zoeames@gmail.com>
2014-09-19 15:35:16 -0500

    user model error fixed

--- test/unit/user.js
+++ test/unit/user.js
@@ -5,8 +5,7 @@ var expect    = require('chai').expect,
     dbConnect = require('../../server/lib/mongodb'),
     cp        = require('child_process'),
     Mongo     = require('mongodb'),
-    db        = 'enlighTN-test',
-    zoe;
+    db        = 'enlighTN-test';
 
 describe('User', function(){
   before(function(done){
@@ -16,17 +15,6 @@ describe('User', function(){
   });
 
   beforeEach(function(done){
-    var z = {
-      '_id' : '000000000000000000000003',
-      'name' : 'Zoe',
-      'email' : 'zoe@aol.com',
-      'password' : '1234',
-      'favoriteLocations':['e00000000000000000000002', 'c0000000000000000000007', 'a00000000000000000000001'],
-      'RSVP':['eee000000000000000000001', 'eee000000000000000000002'],
-      'upVote' : ['aa0000000000000000000002', 'aa0000000000000000000004']
-    };
-    zoe = new User(z);
-
     cp.execFile(__dirname + '/../scripts/clean-db.sh', [db], {cwd:__dirname + '/../scripts'}, function(err, stdout, stderr){
       done();
     });
@@ -46,10 +34,10 @@ describe('User', function(){
   describe('.save', function(){
     it('should save a user', function(done){
       var id = '000000000000000000000002';
-      User.findById(id, function(user){
-        console.log('test', user);
+      User.findById(id, function(err, user){
+        console.log('test1', user);
         user.email='zoea@aol.com';
-        console.log('test', user);
+        console.log('test2', user);
 
         User.save(user, function(err, sobj){
           expect(user._id).to.be.instanceof(Mongo.ObjectID);
