33914d132c5e9580c9cdaa06ac8b5871983c057d
Kayla Jones <KaylaLyne424@gmail.com>
2014-09-22 11:11:09 -0500

    login and register works with the new set up

--- client/views/splash/splash.js
+++ client/views/splash/splash.js
@@ -2,8 +2,39 @@
   'use strict';
 
   angular.module('enlighTN')
-  .controller('SplashCtrl', ['$scope', 'User', function($scope, User){
-    $scope.title = 'Login crap.';
+  .controller('SplashCtrl', ['$scope', 'User', '$location', function($scope, User, $location){
+    //login------------------------------------------
+    $scope.user = {};
+
+    function success(response){
+      toastr.success('Successful login.');
+      $location.path('/home');
+    }
+
+    function failure(response){
+      toastr.error('Error during login, try again.');
+      $scope.user = {};
+    }
+
+    $scope.login = function(){
+      User.login($scope.user).then(success, failure);
+    };
+    //register----------------------------------------
+    $scope.regUser = {};
+
+    function regSuccess(response){
+      toastr.success('User successfully registered.');
+      $location.path('/login');
+    }
+
+    function regFailure(response){
+      toastr.error('Error during user registration, try again.');
+      $scope.regUser = {};
+    }
+
+    $scope.register = function(){
+      User.register($scope.regUser).then(regSuccess, regFailure);
+    };
   }]);
 })();
 
