d0c8070cba6370f1212585fbb4984a96fdb68301
Zoe Ames <zoeames@gmail.com>
2014-10-10 11:53:44 -0500

    back end for I will attend this event

--- server/models/event.js
+++ server/models/event.js
@@ -2,7 +2,8 @@
 
 var Mongo = require('mongodb'),
     async = require('async'),
-    User  = require('./user');
+    User  = require('./user'),
+    underscore = require('underscore');
 
 function Occasion(o){
  /* this.name       = o.name;
@@ -20,6 +21,7 @@ Occasion.findById = function(id, cb){
   console.log(id);
   var _id = Mongo.ObjectID(id);
   Occasion.collection.findOne({_id:_id}, function(err, occasion){
+    occasion.attendees=occasion.attendees || [];
     async.map(occasion.attendees, iterator, function(err, attendees){
       occasion.attendees = attendees;
       console.log('Model:',occasion);
@@ -32,6 +34,32 @@ Occasion.all = function(cb){
   Occasion.collection.find().toArray(cb);
 };
 
+Occasion.retrieve = function(userId, eventId, cb){
+  Occasion.findById(eventId, function(err, occasion){
+    var rsvp = RSVP(occasion.attendees, userId);
+    cb(err, occasion, rsvp);
+  });
+};
+
+Occasion.rsvp = function(userId, eventId, cb){
+  Occasion.findById(eventId, function(err, occasion){
+    occasion.attendees=occasion.attendees.map(unIterator);
+    var rsvp = RSVP(occasion.attendees, userId);
+
+    if(rsvp === true){
+      occasion.attendees = underscore.without(occasion.attendees, userId);
+    }else{
+      occasion.attendees.push(userId);
+    }
+    Occasion.collection.save(occasion, function(){
+      User.rsvp(userId, eventId, function(){
+        cb(null, !rsvp);
+      });
+    });
+  });
+};
+
+
 module.exports = Occasion;
 
 //HELPER FUNCTIONS
@@ -40,4 +68,18 @@ function iterator(attendee, cb){
   User.findById(attendee, function(err, user){
     cb(null, user);
   });
+}
+
+function unIterator(attendee){
+  return attendee._id.toString();
+}
+
+function RSVP(array, userId){
+  if(!underscore.find(array, function(id){
+    return id === userId;
+  })){
+    return false;
+  }else{
+    return true;
+  }
 }
