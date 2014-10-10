429ef7ded964295778293127be0486fa35200fc7
Kayla Jones <KaylaLyne424@gmail.com>
2014-10-10 14:13:24 -0500

    reflections edit forms work on the front end

--- client/views/dashboard/dashboard.js
+++ client/views/dashboard/dashboard.js
@@ -3,10 +3,15 @@
 
   angular.module('enlighTN')
   .controller('DashboardCtrl', ['$scope', function($scope){
-    $scope.user= {
+    $scope.showReflect = {};
+    $scope.user = {
       name: 'Stella Bella',
       email: 'stella@gmail.com'
     };
+    $scope.reflections = [
+      {date:'12/13/14', title:'Wise, if not slightly pedantic.', body:'A kidney of the match is assumed to be a kittle blow. The literature would have us believe that a brainsick spinach is not but a cancer.'},
+      {date:'12/13/14', title:'Wise, if not slightly pedantic.', body:'A kidney of the match is assumed to be a kittle blow. The literature would have us believe that a brainsick spinach is not but a cancer.'}
+    ];
 // Quote Shuffler-------------------------------------------
     $scope.quotes = [
       {body: 'You can\'t just give someone a creativity injection. You have to create an environment for curiosity and a way to encourage people and get the best out of them.', author:'Ken Robinson'},
@@ -28,8 +33,8 @@
       $scope.showEdit = !!!$scope.showEdit;
     };
 
-    $scope.toggleReflect = function(){
-      $scope.showReflect = !!!$scope.showReflect;
+    $scope.toggleReflect = function(index){
+      $scope.showReflect[index] = !!!$scope.showReflect[index];
     };
 
   }]);
